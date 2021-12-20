1028d77c2f608c3c6ee2b2bf8ea53e786ee333c8
sahiba <sahiba1214@gmail.com>
2021-12-20 22:37:22 +0530

    [sahiba1214] add file ProductToList

---
+++ ProductReviewManagement/ProductReviewManagement/Management.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductReviewManagement
+{
+    class Management
+    {
+
+        public void Display(List<ProductReview> productList)
+        {
+            foreach (ProductReview pro in productList)
+            {
+                Console.WriteLine("ProductId : {0} \t UserId : {1} \t Rating : {2} \t Review : {3} \t IsLike : {4}", pro.ProductID, pro.UserID, pro.Rating, pro.Review, pro.IsLike);
+            }
+        }
+    }
+}
+    
